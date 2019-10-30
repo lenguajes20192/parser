bb3aa27b13885eb0482501417c29842a27a9f1ee
Laura Gineth Morales Ariza <lgmoralesa@unal.edu.co>
2019-10-29 22:45:21 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,85 @@ import java.util.*;
 
 public class Main {
 
+    /* _____________________ LEXER _________________________________ */
+
+    static String[] resWords = new String[]{ "af", "and", "binding","by", "body", "cap", "coenter", "col", "create","destroy", "do",
+            "else", "end", "external", "fa", "fi", "file", "final", "get", "getarg", "global", "if",
+            "import", "int", "mod", "new","noop", "oc", "op", "or","procedure", "process", "read",
+            "real", "ref", "res", "resource", "returns", "scanf", "sem", "send","sprintf", "st",
+            "stop", "to", "val", "var", "write", "writes","receive","char","reply","next","string",
+            "bool","ni","co","proc"};
+
+    static String[] symbols = new String[]{"{","}",":=",":",",","[","]",";","(",")","+","++","--","<","<=",">",">=","!=",":=:","->",
+            "-", "[]", "=", "%", ".", "|", "/", "*", "<<", ">>", "||", "**", "<<:=", ">>:=", "||:=", "**:=", "*:=", "/:=", "+:=",
+            "-:=", "%:=", "%", "&:=", "&", "|:="};
+
+    static String[] values = new String[]{"tk_llave_i", "tk_llave_d", "tk_asig", "tk_dos_puntos", "tk_coma", "tk_cor_izq",
+            "tk_cor_der", "tk_punto_y_coma", "tk_par_izq", "tk_par_der", "tk_suma", "tk_incr", "tk_decr", "tk_menorque",
+            "tk_menor_igual", "tk_mayorque", "tk_mayor_igual", "tk_distinto", "tk_swap", "tk_ejecuta",
+            "tk_menos", "tk_separa", "tk_igual", "tk_mod","tk_punto","tk_or", "tk_div", "tk_multi", "tk_lshift", "tk_rshift",
+            "tk_concat", "tk_exp", "tk_lsasig", "tk_rsasig", "tk_concasig", "tk_expasig", "tk_mulasig", "tk_divasig", "tk_sumasig",
+            "tk_resasig", "tk_remasig", "tk_rem", "tk_andasig", "tk_and", "tk_orasig"};
+
+    static HashSet<String> h = new HashSet<String>();
+    static HashMap<String, String> mpLexer = new HashMap<String, String>();
+
+    public static class Token {
+        public int row;
+        public int column;
+        public String lexeme;
+        public int type;
+
+        public Token(int type, String lexemeValue, int rowValue, int columnValue){
+            this.type= type;
+            lexeme = lexemeValue;
+            row = rowValue;
+            column = columnValue;
+        }
+
+        public void printTok(){
+            switch(type){
+                case 1:
+                    if(h.contains(lexeme))// if reserved
+                        System.out.println("<" + lexeme+","+ row + ","+ column+">");
+                    else
+                        System.out.println("<id," + lexeme+"," + row + "," + column + ">");
+
+                    break;
+
+                case 2://belong to symbols
+                    System.out.println("<" + mpLexer.get(lexeme) + "," + row + "," + column + ">");
+                    break;
+
+                case 3://is number
+                    System.out.println("<tk_num," + lexeme + "," + row + "," + column + ">");
+                    break;
+
+                case 4://is string
+                    System.out.println("<tk_cadena," + lexeme + "," + row + "," + column + ">");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    static void init() {
+        h.addAll(Arrays.asList(resWords));
+        //System.out.println("No match")
+        for (int i = 0; i < symbols.length; i++) {
+            mpLexer.put(symbols[i], values[i]);
+        }
+    }
+
+    static void printError(int row, int column){
+        System.out.println(">>>Error_lexico(linea:" + row + ",posicion:" + (column + 1) + ")");
+    }
+
+    /* _____________________ LEXER END _________________________________ */
+
+
     private static HashMap<String, String[][]> mp = new HashMap<>();
     private static ArrayList<String>  arrOfKeys = new ArrayList<>();
     private static Deque<String> keys = new LinkedList<>();
@@ -71,7 +150,7 @@ public class Main {
                 for(int j = 0; j < values.length; j++){
                     List<String> valuesList = Arrays.asList(values[j]);
                     if(valuesList.contains(keyFollow)){
-                        if(valuesList.indexOf(keyFollow) == valuesList.size()-1){
+                        if(valuesList.indexOf(keyFollow) == valuesList.size()-1){ //el que se esta evaluando es el ultimo
                             sig.addAll(follow.get(keyRule));
                         }
                         else if (!keys.contains(valuesList.get(valuesList.indexOf(keyFollow)+1))){  //el contiguo es un T
@@ -120,6 +199,13 @@ public class Main {
         }
         System.out.println(Arrays.asList(pred));
 
+    }
+
+    void emparejar(TElementoLexico tokEsperado){
+        if (token == tokEsperado)
+            token = lexico.getNextToken();
+        else
+            errorSintaxis(tokEsperado);
     }
 
     public static void main(String [] args){
